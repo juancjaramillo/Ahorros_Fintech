ffb12e51ba0b719db02c1fd8a4699207f0a88ef1
Juan Carlos Jaramillo <ingenierojramillo@gmail.com>
2025-08-08 09:45:18 -0500

    chore: ignore zips and env files

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,19 @@
-# backend/main.py
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 
-# ─── imports propios ─────────────────────────────────────────────────────────
 from backend.database import Base, engine
 from backend.routers  import auth, account, transaction, user
-# ─────────────────────────────────────────────────────────────────────────────
 
 app = FastAPI(
     title="Sistema de Ahorros Fintech",
     version="1.0",
     description="API segura para manejo de cuentas de ahorro",
-    docs_url="/api/docs",              #  mover Swagger a /api/docs
-    openapi_url="/api/openapi.json",   #  y el JSON a /api/openapi.json
-    redoc_url=None,
+    docs_url="/api/docs",              # <── importante
+    openapi_url="/api/openapi.json",   # <── importante
+    redoc_url=None, z
 )
 
-# ─── CORS: permite que el frontend (3000) hable con la API (8000) ────────────
-# CORS solo en desarrollo (no en prod)
 ENV = os.getenv("ENV", "prod")
 if ENV == "dev":
     app.add_middleware(
@@ -27,15 +23,12 @@ if ENV == "dev":
         allow_methods=["*"],
         allow_headers=["*"],
     )
-# ─────────────────────────────────────────────────────────────────────────────
 
-# crea tablas (si no existen) en la BD: C:\xampp82\htdocs\ahorro_app\ahorros.db
 Base.metadata.create_all(bind=engine)
 
-# ─── Routers ─────────────────────────────────────────────────────────────────
-app.include_router(auth.router)        # /auth/…
-app.include_router(user.router)        # /users/…  (solo admin)
-app.include_router(account.router)     # /accounts/…
-app.include_router(transaction.router) # /transactions/…
-
-# ─────────────────────────────────────────────────────────────────────────────
+api_router = APIRouter(prefix="/api")  # <── importante
+api_router.include_router(auth.router)
+api_router.include_router(user.router)
+api_router.include_router(account.router)
+api_router.include_router(transaction.router)
+app.include_router(api_router)
