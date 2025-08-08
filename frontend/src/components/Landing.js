09a361ef020fe04feebc1f510cd479ca180a2c31
Juan Carlos Jaramillo <ingenierojramillo@gmail.com>
2025-08-07 22:46:17 -0500

    prueba con AWS

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Landing() {
   return (
     <div className="d-flex flex-column align-items-center pt-5">
-      <h2 className="mb-4">Bienvenido al Sistema de Ahorros - </h2>
+      <h2 className="mb-4">Bienvenido al Sistema de Ahorros  </h2>
       <div className="d-flex gap-4">
         <Link to="/client-login" className="btn btn-success btn-lg px-5">
           Soy Cliente
